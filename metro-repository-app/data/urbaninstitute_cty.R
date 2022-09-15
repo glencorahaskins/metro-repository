2781f29b7a798a4fde83e0b255dbed5cabfba715
glencorahaskins <ghaskins@brookings.edu>
2022-09-15 13:42:55 -0400

    Update urbaninstitute_cty.R

--- metro-repository-app/data/urbaninstitute_cty.R
+++ metro-repository-app/data/urbaninstitute_cty.R
@@ -40,6 +40,22 @@ debt_overall <- read_excel("county_dia_delinquency_7 June 2022.xlsx")
 file.remove("county_dia_delinquency_7 June 2022.xlsx")
 rm("link_overall")
 
+debt_auto <- debt_auto %>%
+  rename(stco_code = GEOID) %>%
+  rename(stco_name = NAME)
+
+debt_med <- debt_med %>%
+  rename(stco_code = GEOID) %>%
+  rename(stco_name = NAME)
+
+debt_student <- debt_student %>%
+  rename(stco_code = GEOID) %>%
+  rename(stco_name = NAME)
+
+debt_overall <- debt_overall %>%
+  rename(stco_code = GEOID) %>%
+  rename(stco_name = NAME)
+
 oldw <- getOption("warn")
 
 options(warn=-1)
@@ -49,33 +65,31 @@ debt_student[ ,-c(1:3)] <- sapply(debt_student[ ,-c(1:3)], as.numeric)
 debt_overall[ ,-c(1:3)] <- sapply(debt_overall[ ,-c(1:3)], as.numeric)
 options(warn = oldw)
 
-list_auto <- names(auto)
-list_med <- names(med)
-list_student <- names(student)
-list_overall <- names(overall)
-
 rm(oldw)
 
-debt <- debt_overall
+list_auto <- names(debt_auto)
+list_med <- names(debt_med)
+list_student <- names(debt_student)
+list_overall <- names(debt_overall)
 
-common <- intersect(names(debt), names(debt_overall))
-debt <- merge(debt, debt_overall, by=common, all.x=TRUE)
+debt_cty <- debt_overall
+
+common <- intersect(names(debt_cty), names(debt_overall))
+debt_cty <- merge(debt_cty, debt_overall, by=common, all.x=TRUE)
 rm(common)
 
-common <- intersect(names(debt), names(debt_auto))
-debt <- merge(debt, debt_auto, by=common, all.x=TRUE)
-rm(common)
-
-common <- intersect(names(debt), names(debt_med))
-debt <- merge(debt, debt_med, by=common, all.x=TRUE)
-rm(common)
-
-common <- intersect(names(debt), names(debt_student))
-debt <- merge(debt, debt_student, by=common, all.x=TRUE)
+common <- intersect(names(debt_cty), names(debt_auto))
+debt_cty <- merge(debt_cty, debt_auto, by=common, all.x=TRUE)
 rm(common)
 
 
+common <- intersect(names(debt_cty), names(debt_med))
+debt_cty <- merge(debt_cty, debt_med, by=common, all.x=TRUE)
+rm(common)
 
+common <- intersect(names(debt_cty), names(debt_student))
+debt_cty <- merge(debt_cty, debt_student, by=common, all.x=TRUE)
+rm(common)
 
 
 
