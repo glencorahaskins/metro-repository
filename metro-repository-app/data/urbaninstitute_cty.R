f4c6a0673a8531747f1d526ac38c533f40660262
glencorahaskins <ghaskins@brookings.edu>
2022-09-15 13:29:18 -0400

    Create urbaninstitute_cty.R

---
+++ metro-repository-app/data/urbaninstitute_cty.R
@@ -0,0 +1,86 @@
+library(censusapi)
+library(data.table)
+library(dplyr)
+library(DT)
+library(ggplot2)
+library(markdown)
+library(plotly)
+library(readr)
+library(rJava)
+library(shiny)
+library(stringr)
+library(sqldf)
+library(tidycensus)
+library(tidyverse)
+library(writexl)
+library(readxl)
+library(xlsx)
+
+link_auto <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2022/06/16/county_dia_auto_%207%20Jun%202022.xlsx"
+download.file(link_auto, "county_dia_auto_7 June 2022.xlsx", quiet = TRUE, mode = "wb")
+debt_auto <- read_excel("county_dia_auto_7 June 2022.xlsx")
+file.remove("county_dia_auto_7 June 2022.xlsx")
+rm("link_auto")
+
+link_med <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2022/06/16/county_dia_medical_%207%20Jun%202022.xlsx"
+download.file(link_med, "county_dia_medical_7 June 2022.xlsx", quiet = TRUE, mode = "wb")
+debt_med <- read_excel("county_dia_medical_7 June 2022.xlsx")
+file.remove("county_dia_medical_7 June 2022.xlsx")
+rm("link_med")
+
+link_student <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2022/06/16/county_dia_student_%207%20Jun%202022.xlsx"
+download.file(link_student, "county_dia_student_7 June 2022.xlsx", quiet = TRUE, mode = "wb")
+debt_student <- read_excel("county_dia_student_7 June 2022.xlsx")
+file.remove("county_dia_student_7 June 2022.xlsx")
+rm("link_student")
+
+link_overall <- "https://urban-data-catalog.s3.amazonaws.com/drupal-root-live/2022/06/16/county_dia_delinquency_%207%20Jun%202022.xlsx"
+download.file(link_overall, "county_dia_delinquency_7 June 2022.xlsx", quiet = TRUE, mode = "wb")
+debt_overall <- read_excel("county_dia_delinquency_7 June 2022.xlsx")
+file.remove("county_dia_delinquency_7 June 2022.xlsx")
+rm("link_overall")
+
+oldw <- getOption("warn")
+
+options(warn=-1)
+debt_auto[ ,-c(1:3)] <- sapply(debt_auto[ ,-c(1:3)], as.numeric)
+debt_med[ ,-c(1:3)] <- sapply(debt_med[ ,-c(1:3)], as.numeric)
+debt_student[ ,-c(1:3)] <- sapply(debt_student[ ,-c(1:3)], as.numeric)
+debt_overall[ ,-c(1:3)] <- sapply(debt_overall[ ,-c(1:3)], as.numeric)
+options(warn = oldw)
+
+list_auto <- names(auto)
+list_med <- names(med)
+list_student <- names(student)
+list_overall <- names(overall)
+
+rm(oldw)
+
+debt <- debt_overall
+
+common <- intersect(names(debt), names(debt_overall))
+debt <- merge(debt, debt_overall, by=common, all.x=TRUE)
+rm(common)
+
+common <- intersect(names(debt), names(debt_auto))
+debt <- merge(debt, debt_auto, by=common, all.x=TRUE)
+rm(common)
+
+common <- intersect(names(debt), names(debt_med))
+debt <- merge(debt, debt_med, by=common, all.x=TRUE)
+rm(common)
+
+common <- intersect(names(debt), names(debt_student))
+debt <- merge(debt, debt_student, by=common, all.x=TRUE)
+rm(common)
+
+
+
+
+
+
+
+
+
+
+
