97e37a2aff6c815f4020205965165ca2e3e74b72
glencorahaskins <ghaskins@brookings.edu>
2022-09-21 10:56:23 -0400

    Create metro-packages.R

---
+++ metro-functions/metro-packages.R
@@ -0,0 +1,12 @@
+metro.packages <- function(metro.packages) {
+  packages <- c('censusapi', 'data.table', 'devtools', 'dplyr', 'DT', 'formattable', 'ggplot2', 'httr', 'jsonlite', 'librarian', 'lubridate', 'markdown', 'metro.data', 'openxlsx', 'pacman', 'plotly', 'readr', 'readxl', 'rJava', 'shiny', 'sqldf', 'stringr', 'tidycensus', 'tidyr', 'tidyverse', 'writexl', 'xlsx', 'XML')
+  
+  installed_packages <- packages %in% rownames(installed.packages())
+  if (any(installed_packages == FALSE)) {
+    install.packages(packages[!installed_packages])
+  }
+  invisible(lapply(packages, library, character.only = TRUE))
+  
+  rm(installed_packages)
+  rm(packages)
+}
